5640c11cd5cdbb3b80b28f47f9f0f7ddf7f17266
Anthony Poblacion <StBean@users.noreply.github.com>
2018-06-29 12:35:36 -0700

    Update InsertionSort.js

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,1 +1,14 @@
-
+function InsertionSort(data){
+	for(var i = 0; i < data.length - 1; i++){
+		var min = i; 
+		for(var j = i + 1; j < (data.length); j++){
+			if(data[j] < data[min]){
+				min = j;
+			}
+			var temp = data[i];
+			data[i] = data[min];
+			data[min] = temp;
+		}
+	}
+	return data;
+};
